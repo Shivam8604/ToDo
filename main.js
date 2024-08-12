a238c35166f50538b3d99cc216a2111082b58008
shivam pal <devshivampal360@gmail.com>
2024-08-12 11:04:00 +0530

    add card

---
+++ main.js
@@ -0,0 +1,42 @@
+// console.log("hello shiv ")
+
+// var state = {
+//     taskList:[
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         },
+//     ]
+// }
+
+var state = {
+    taskList:[]
+};
+
+// DOM Operation 
+
+var taskContent = document.querySelector(".task_content");
+var taskModal =document.querySelector(".task-modal-body");
+
+console.log(taskContent);
+console.log(taskModal);

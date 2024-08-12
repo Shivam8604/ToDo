ed5c9593da82b1a4575a15c88f0b0ca3bc0a573e
shivam pal <devshivampal360@gmail.com>
2024-08-12 12:31:30 +0530

    update localstorage

--- main.js
+++ main.js
@@ -40,3 +40,54 @@ var taskModal =document.querySelector(".task-modal-body");
 
 console.log(taskContent);
 console.log(taskModal);
+
+
+
+const htmlTaskContent = ({id,title,description,type,url})=>`
+    <div class="col-md-6 col-lg-4 mt-3 id=${id} key=${id}">
+        <div class="card shadow-sm task_card">
+            <div class="card-header d-flex justify-content-end task_card_header gap-2">
+                <button type="button" class="btn btn-outline-info mr-2" name=${id}>
+                    <i class="fas fa-pencil-alt" name=${id}></i>
+                </button>
+                <button type="button" class="btn btn-outline-dark mr-2" name=${id}>
+                    <i class="fas fa-trash-alt" name=${id}></i>
+                </button>
+            </div>
+            <div class="card-body">
+                ${
+                    url && `<img src=${url} alt="card image top" class="card-image-top md-3 rounded-lg"/>`
+                }
+                <h4 class="card-title">${title}</h4>
+                <p class="description trim-3-lines text-muted card-text">${description}</p>
+                <div class="tags text-white d-flex flex-wrap bg-primary m-1">
+                    <span class="badge bg-primary m-2">${type}</span>
+                </div>
+            </div>
+            <div class="card-footer">
+                <button type="button" class="btn btn-outline-primary float-right" data-bs-toggle="modal" data-bs-target="#showTask">Open Task</button>
+            </div>
+        </div>
+    </div>
+`
+
+const htmlModalContent = ({id,title,description,url})=>{
+    const date = new Date(paraInt(id));
+    return `
+        <div id=${id}>
+            ${
+                url && `<img src=${url} alt="card image cap" class="image-fluid md-3"/>`
+            }
+            <strong>Created on ${data.toDoteString()}</strong>  
+            <h2 clss="my-3">${title}</h2>
+            <p class="lead">${description}</p>
+        </div>
+    `
+}
+
+
+const updateLocalStorage = () =>{
+    localStorage.setItem("task",JSON.stringify({
+        tasks: state.taskList,
+    }))
+}
